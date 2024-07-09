712ec372890c7d48b39eef6f076a1d9c9a26161a
haleqiu <yuhengq@andrew.cmu.edu>
2024-07-08 20:53:25 -0400

    update the script

--- bag_msg/saveImage_bag.py
+++ bag_msg/saveImage_bag.py
@@ -6,30 +6,27 @@ import rosbag
 from os.path import isfile, join, dirname, isdir
 from os import listdir, mkdir
 import argparse
+import glob, tqdm
 
 # Arguements
 parser = argparse.ArgumentParser(description='save ros bag')
-parser.add_argument("--topics", type=str, default=['/cam_image/1/scene', '/cam_image/2/scene'], help="topic", nargs= "+")## multiple
-parser.add_argument("--outdir", type=str, default='/data/dynamic_person/', help="where to save the txt")
-parser.add_argument("--inputdir", type=str, default='/data/dynamic_person/', help="the folder for input bag file")
-parser.add_argument("--bag", type=str, default='/data/dynamic_person/bag/2020-08-16-23-33-27.bag', help="the folder for input bag file")
+parser.add_argument("--topics", type=str, default=['/zedx/zed_node/left/image_rect_color', '/zedx/zed_node/right/image_rect_color'], help="topic", nargs= "+")## multiple
+parser.add_argument("--outdir", type=str, default='/home/yuhneg/data/dsta_payload_2024-07-08-11-24-41_1/', help="where to save the txt")
+parser.add_argument("--inputdir", type=str, default='/home/yuhneg/data/dsta_payload_bag', help="the folder for input bag file")
 args = parser.parse_args(); print(args)
-
 
 # outvidfile = 'bag_save6.avi'
 skip = 1
 cvbridge = CvBridge()
 SaveVideo = False
-image_size = (960,720)
+image_size = (960,600)
 
 
-for filename in listdir(join(args.inputdir, "bag")):
+for filename in glob.glob(args.inputdir + "/*.bag"):
     print(filename)
     # save multiple bagfiles in one folder into one single video
 
     filepathname = join(args.inputdir, "bag", filename)
-    if (not isfile(filepathname)) or (not filename[-3:]=='bag'):
-        continue
 
     local_path = join(args.inputdir,filename.split('.')[0])
     if not isdir(local_path):
@@ -43,17 +40,15 @@ for filename in listdir(join(args.inputdir, "bag")):
         fout=cv2.VideoWriter(outvidfile, fourcc, 30.0, image_size)
 
     bag = rosbag.Bag(filepathname, 'r')
-    subfolder = "image_1"## TODO cam1 is right image in airsim hhh
+    subfolder = "image_0"## TODO cam1 is right image in airsim hhh
     for t in args.topics:
+        print(t)
         ind = 0
         time_txt = open(join(local_path, subfolder,"times.txt"), "a")
-        for topic, msg, t in bag.read_messages(topics=t):
+        for info in tqdm.tqdm(bag.read_messages(topics=t)):
+            topic, msg, t = info
             if ind%skip==0:
-                image_np = cvbridge.imgmsg_to_cv2(msg, "rgb8")  # TODO: check the encoding
-                    # cv2.imshow('img', image_np)
-                    # cv2.waitKey(0)
-                    # image_np = cv2.resize(image_np, (640, 360)) # Uncomment if you want to resize the image
-                    # print image_np.shape
+                image_np = cvbridge.imgmsg_to_cv2(msg, "bgr8")  # TODO: check the encoding
                 if SaveVideo:
                     fout.write(image_np)
                 else:
@@ -62,7 +57,7 @@ for filename in listdir(join(args.inputdir, "bag")):
                     time_stamp = str(msg.header.stamp.secs) + "." + str(msg.header.stamp.nsecs)
                     time_txt.write(time_stamp + "\n")
             ind = ind + 1
-        subfolder = "image_0"
+        subfolder = "image_1"
         time_txt.close()
     bag.close()
 

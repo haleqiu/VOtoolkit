64b166b028266ff2e2b57061c31e905f864a189f
haleqiu <yuhengq@andrew.cmu.edu>
2024-07-19 16:03:42 -0400

    update the script for stereo update

--- rectify_stereo.py
+++ rectify_stereo.py
@@ -34,50 +34,31 @@ def parse_intrinsics(cam_calibration):
 
     return np.array(xi), to_K(fx, fy, cx, cy), np.array(dist)
 
-if __name__ == "__main__":
-    base_folder = "/home/yuhneg/data/dsta_payload"
-    out_folder = os.path.join(base_folder, "cam45")
-    calibration_file = os.path.join(base_folder, "cam4-5.yaml")
-    left_image_folder = os.path.join(base_folder, "frames_4")
-    right_image_folder = os.path.join(base_folder, "frames_5")
-
-    with open(calibration_file, "r") as f:
-        params = yaml.safe_load(f)
-
-    xi1, K1, D1 = parse_intrinsics(params["cam0"])
-    xi2, K2, D2 = parse_intrinsics(params["cam1"])
-    ##### left is cam1, right is cam0
-
-    E = pp.mat2SE3(np.array(params["cam1"]["T_cn_cnm1"]))
-    TCN = E.matrix().numpy()
-
-    W, H = params["cam0"]["resolution"]
-
+def omin_stereo_rectify(left_image_folder, right_image_folder, left_params, right_params, TCN, out_folder):
+    xil, Kl, Dl = parse_intrinsics(left_params)
+    xir, Kr, Dr = parse_intrinsics(right_params)
+    
     R = TCN[:3,:3]
     T = TCN[:3,3]
 
-    # Stereo rectification
-    RL, RR = cv2.omnidir.stereoRectify(
-        R, T
-    )
-
+    RL, RR = cv2.omnidir.stereoRectify(R, T)
+    
     image_size = (600, 600)
-
+    
     P = np.array([
         [200, 0, 300],
         [0, 200, 300],
         [0, 0, 1]
     ], dtype=np.float32)
-
+    
     # Undistort and rectify the images
-    map1_x, map1_y = cv2.omnidir.initUndistortRectifyMap(
-        K1, D1, xi1, RL, P, image_size, cv2.CV_32FC1, cv2.omnidir.RECTIFY_PERSPECTIVE
+    mapl_x, mapl_y = cv2.omnidir.initUndistortRectifyMap(
+        Kl, Dl, xil, RL, P, image_size, cv2.CV_32FC1, cv2.omnidir.RECTIFY_PERSPECTIVE
     )
-    map2_x, map2_y = cv2.omnidir.initUndistortRectifyMap(
-        K2, D2, xi2, RR, P, image_size, cv2.CV_32FC1, cv2.omnidir.RECTIFY_PERSPECTIVE
+    mapr_x, mapr_y = cv2.omnidir.initUndistortRectifyMap(
+        Kr, Dr, xir, RR, P, image_size, cv2.CV_32FC1, cv2.omnidir.RECTIFY_PERSPECTIVE
     )
-
-
+    
     files_left = glob.glob(os.path.join(left_image_folder, "*.jpg"))
     files_right = glob.glob(os.path.join(right_image_folder, "*.jpg"))
     files_left.sort()
@@ -87,11 +68,46 @@ if __name__ == "__main__":
         imgl = cv2.imread(file_left)
         imgr = cv2.imread(file_right)
 
-        rectified_img0, rectified_img1 = recity_Stereo(map1_x, map1_y, map2_x, map2_y, imgl, imgr)
+        rectified_imgl = cv2.remap(imgl, mapl_x, mapl_y, cv2.INTER_LINEAR)
+        rectified_imgr = cv2.remap(imgr, mapr_x, mapr_y, cv2.INTER_LINEAR)
 
         # plot_rect_img(img1, img0)
         os.makedirs(os.path.join(out_folder, "rgb_l"), exist_ok=True)
         os.makedirs(os.path.join(out_folder, "rgb_r"), exist_ok=True)
         # 0 is right camera
-        cv2.imwrite(os.path.join(out_folder, "rgb_l", os.path.basename(file_left)), rectified_img1)
-        cv2.imwrite(os.path.join(out_folder, "rgb_r", os.path.basename(file_right)), rectified_img0)
+        cv2.imwrite(os.path.join(out_folder, "rgb_l", os.path.basename(file_left)), rectified_imgl)
+        cv2.imwrite(os.path.join(out_folder, "rgb_r", os.path.basename(file_right)), rectified_imgr)
+
+
+if __name__ == "__main__":
+    base_folder = "/home/yuhneg/data/dsta_payload/07-17"
+    
+    
+    # for cam 01
+    out_folder = os.path.join(base_folder, "cam01")
+    calibration_file = os.path.join(base_folder, "cam0-1.yaml")
+    left_image_folder = os.path.join(base_folder, "frames_1")
+    right_image_folder = os.path.join(base_folder, "frames_0")
+
+    with open(calibration_file, "r") as f:
+        params = yaml.safe_load(f)
+        
+    E = pp.mat2SE3(np.array(params["cam1"]["T_cn_cnm1"]))
+    
+    omin_stereo_rectify(left_image_folder, right_image_folder, left_params=params["cam1"], right_params=params["cam0"], TCN= E.Inv().matrix().numpy(), out_folder=out_folder)
+    
+    # for cam 45
+    out_folder = os.path.join(base_folder, "cam45")
+    calibration_file = os.path.join(base_folder, "cam4-5.yaml")
+    left_image_folder = os.path.join(base_folder, "frames_4")
+    right_image_folder = os.path.join(base_folder, "frames_5")
+
+    with open(calibration_file, "r") as f:
+        params = yaml.safe_load(f)
+        
+    E = pp.mat2SE3(np.array(params["cam1"]["T_cn_cnm1"]))
+    
+    omin_stereo_rectify(left_image_folder, right_image_folder, left_params=params["cam0"], right_params=params["cam1"], TCN= E.matrix().numpy(), out_folder=out_folder)
+
+
+    
